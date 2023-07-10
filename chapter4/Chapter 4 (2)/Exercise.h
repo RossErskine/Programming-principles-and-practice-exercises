945aae9ccd7de16278529c1c2ba99c36e6ee62a6
Ross.Erskine <46631932+RossErskine@users.noreply.github.com>
2023-07-10 08:02:35 +0100

    Exercise files added

--- chapter4/Chapter 4 (2)/Exercise.h
+++ chapter4/Chapter 4 (2)/Exercise.h
@@ -1,23 +1,33 @@
 #pragma once
 #include "Word_Calculator.h"
-#include "myVector.h"
+#include "RiceGrain.h"
 #include "Quadratic_formula.h"
+#include "Vector.h"
+#include "names_n_scores.h"
+
 
 class Exercise
 {
-public: 
-	void runExercises();
+public:
+	Exercise();
+	~Exercise();
+	void runExercise();
+	
+
+private:
 	void exercise_6();
+	void exercise_10();
 	void exercise_11();
 	bool find_prime(int n);
 	void exercise_18();
 	void exercise_19();
 
-	Word_Calculator wc;
-	myVector<int> IVector;
+	Word_Calculator word_calc;
+	RiceGrain rg;
+	//RockPaperScissors rps;
+	Vector IVector;
 	Quadratic_formula qf;
-	myVector<string> names;
-	myVector<int> scores;
+	names_n_scores ns;
 	
 };
 
